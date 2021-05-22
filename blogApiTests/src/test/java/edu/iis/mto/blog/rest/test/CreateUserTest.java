0bd74d4cd977203f22e35a8d6c9a05d6d381832f
michalkedzia <222463@edu.p.lodz.pl>
2021-05-22 20:00:29 +0200

    Adding a declaration tests methods

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -25,4 +25,68 @@ class CreateUserTest extends FunctionalTests {
                .when()
                .post(USER_API);
     }
+
+    @Test
+    void createTwoUsersWithSameEmailShouldReturnCode409() {
+        JSONObject jsonObj = new JSONObject().put("email", "unique@emial.com");
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CREATED)
+                .when()
+                .post(USER_API);
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CONFLICT)
+                .when()
+                .post(USER_API);
+    }
+
+    @Test
+    void userWithCONFIRMEDStatusShouldBeAbleToAddNewPost() {
+
+    }
+
+    @Test
+    void userWithCONFIRMEDStatusShouldBeAbleToLikePost() {
+
+    }
+
+    @Test
+    void userShouldNotBeAbleToLikeYourOwnPost() {
+
+    }
+
+    @Test
+    void userShouldBeAbleToLikeYourOwnPost() {
+
+    }
+
+    @Test
+    void addLikeToPostAgainShouldNotChangePostStatus() {
+
+    }
+
+    @Test
+    void postsByUserWithTheDeletedStatusShouldNotBeSuccessful() {
+
+    }
+
+    @Test
+    void postsShouldReturnTheCorrectNumberOfLikes() {
+
+    }
+
+    @Test
+    void deletedUsersShouldNotBeReturned() {
+
+    }
+
 }
